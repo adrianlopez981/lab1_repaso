621ca755b00bcd8770849f2ba86207e5dda96fca
adrianlopez981 <adrian.lopez@pucp.edu.pe>
2023-07-01 17:51:25 -0500

    fist commit

---
+++ src/main/java/com/example/lab1_repaso/Lab1RepasoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1_repaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1RepasoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1RepasoApplication.class, args);
+    }
+
+}
